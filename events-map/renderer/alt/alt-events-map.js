98865cc41cc03e97ae16b6e2638509ed8d976fd0
eparovyshnaia <elena.parovyshnaia@arsysop.ru>
2024-07-05 07:57:07 +0300

    Placeholders and alt events-map Yesterday progress. By AParovyshnaya, anastasya.parovyshnaya@gmail.com

---
+++ events-map/renderer/alt/alt-events-map.js
@@ -0,0 +1,43 @@
+const container = document.getElementById("alternative-events-map");
+//container.replaceChildren(generateAltEventsMap());
+const towns = require("events-map/resources/towns.events");
+console.log(towns);
+
+function createElementAndSetClasses(name, classes) {
+  const element = document.createElement(name);
+  name.setAttribute("class", classes);
+  return name;
+}
+
+function describeEvents() {
+
+
+}
+
+function describe(event) {
+  // const host = createDescribeContainer();
+  // let eventProperties;
+  // host.append(eventProperties);
+}
+
+
+
+function getData(path) {
+}
+
+// function getData(path) {
+//   fetch(path)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Ой, ошибка в fetch: ' + response.statusText);
+//       }
+//       return response.json();
+//     })
+//     .then(jdata => {
+//       for (const event of jdata) {
+//         describe(event);
+//       }
+//     })
+//     .catch(error => console.error('Ошибка при исполнении запроса: ', error));
+//   // 	const sample = require('./events-map/resources/roads.events');
+// }
